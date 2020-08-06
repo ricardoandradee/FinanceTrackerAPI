b2334e19930352a31b9fb035986497d8dbf44d4e
Ricardo Novais <ricardoo.developer@gmail.com>
2020-08-06 13:34:27 +0200

    CanBeDeleted initialized inside constructor

--- FinanceTracker.API/FinanceTracker.Business/Dtos/CategoryToReturnDto.cs
+++ FinanceTracker.API/FinanceTracker.Business/Dtos/CategoryToReturnDto.cs
@@ -4,10 +4,15 @@ namespace FinanceTracker.Business.Dtos
 {
     public class CategoryToReturnDto
     {
+        public CategoryToReturnDto()
+        {
+            CanBeDeleted = false;
+        }
+
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public DateTime? CreatedDate { get; set; }
-        public bool CanBeDeleted { get; set; } = false;
+        public bool CanBeDeleted { get; set; }
     }
 }
