64249860da4a0b5a457dba35f3c6986fbee79a5d
Ricardo Novais <ricardoo.developer@gmail.com>
2020-08-09 12:42:05 +0200

    Email and username fields fix

--- FinanceTracker.SPA/src/app/errorMatchers/error-username-already-taken.matcher.ts
+++ FinanceTracker.SPA/src/app/errorMatchers/error-username-already-taken.matcher.ts
@@ -1,16 +1,17 @@
-import { FormControl } from '@angular/forms';
+import { FormControl, NgForm, FormGroupDirective } from '@angular/forms';
 import { ErrorStateMatcher } from '@angular/material';
 
 export class ErrorUserNameAlreadyTakenMatcher implements ErrorStateMatcher {
   constructor(private userNames: string[]) { }
-  isErrorState(control: FormControl): boolean {
+  isErrorState(control: FormControl, form: FormGroupDirective | NgForm): boolean {
     const inputValue = control && control.value ? control.value.toString() : '';
     const isFormValid = control && control.dirty && control.touched && this.userNames.indexOf(inputValue) > -1;
 
     if (isFormValid) {
-      control.setErrors({ 'userNameAlreadyTaken': true });
+      control.setErrors({ userNameAlreadyTaken: true });
     }
 
-    return isFormValid;
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
   }
 }
