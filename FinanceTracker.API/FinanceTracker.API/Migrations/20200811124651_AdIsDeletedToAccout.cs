baeb9bfee7fe39122fbc4435ee455cb27be3d924
Ricardo Novais <ricardoo.developer@gmail.com>
2020-08-11 19:17:00 +0200

    IsDeleted database changes

---
+++ FinanceTracker.API/FinanceTracker.API/Migrations/20200811124651_AdIsDeletedToAccout.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FinanceTracker.API.Migrations
+{
+    public partial class AdIsDeletedToAccout : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsDeleted",
+                table: "Banks",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Accounts",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Accounts");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsDeleted",
+                table: "Banks",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldDefaultValue: false);
+        }
+    }
+}
