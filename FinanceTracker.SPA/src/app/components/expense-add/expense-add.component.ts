24a993291c6ddcac9036593e106b0db03ab838fa
Ricardo Novais <ricardoo.developer@gmail.com>
2021-07-09 18:59:44 -0300

    Include payment status and bank accounts dropdown

--- FinanceTracker.SPA/src/app/components/expense-add/expense-add.component.ts
+++ FinanceTracker.SPA/src/app/components/expense-add/expense-add.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
-import { MatDialogRef } from '@angular/material';
+import { MatDialogRef, MatRadioChange } from '@angular/material';
 import { Category } from '../../models/category.model';
 import { NgForm } from '@angular/forms';
 import { Expense } from '../../models/expense.model';
@@ -8,6 +8,8 @@ import { Subscription } from 'rxjs';
 import { CommonService } from 'src/app/services/common.service';
 import { Currency } from 'src/app/models/currency.model';
 import { User } from 'src/app/models/user.model';
+import { BankAccountService } from 'src/app/services/bank-account.service';
+import { KeyValuePair } from 'src/app/models/key-value-pair.model';
 
 @Component({
   selector: 'app-expense-add',
@@ -15,31 +17,60 @@ import { User } from 'src/app/models/user.model';
   styleUrls: ['./expense-add.component.scss']
 })
 export class ExpenseAddComponent implements OnDestroy, OnInit {
-  private subscription: Subscription;
+  private allSubscriptions: Subscription[] = [];
   @ViewChild('addExpenseForm', { static: false }) form: NgForm
+  paymentStatuses: string[] = ['Paid', 'Unpaid'];
+  bankAccountsKeyValue: KeyValuePair<string, string>[];
   expense: Expense;
   categories: Category[] = [];
   currencies: Currency[];
 
   constructor(private dialogRef: MatDialogRef<ExpenseAddComponent>,
-              private commonService: CommonService, private categoryService: CategoryService) {
+              private commonService: CommonService,
+              private categoryService: CategoryService,
+              private bankAccountService: BankAccountService) {
     
-    this.subscription = this.categoryService.getCategories.subscribe((categoryList: Category[]) => {
+    var categorySubscription = this.categoryService.getCategories.subscribe((categoryList: Category[]) => {
       this.categories = categoryList;
     });
     
-    this.commonService.getAllCurrencies.subscribe(x => {
+    var currencySubscription = this.commonService.getAllCurrencies.subscribe(x => {
       this.currencies = x;
     });
+
+    this.allSubscriptions.push(categorySubscription);
+    this.allSubscriptions.push(currencySubscription);
   }
   
   ngOnInit() {
     let userSettings = JSON.parse(localStorage.getItem('user')) as User;
     this.expense = {
       establishment: "",
+      status: 'Paid',
       category: { id: 0 } as Category,
       currency: userSettings.currency
     } as Expense;
+    
+    const bankAccountSubscription = this.bankAccountService.getBankAccountInfos.subscribe(bank => {
+      this.bankAccountsKeyValue = [];
+      for (let b of bank) {
+        let bankName = b.name;
+        for (let a of b.accounts) {
+          this.bankAccountsKeyValue.push({
+            key: a.id.toString(),
+            value: bankName + ' - ' + a.number
+          } as KeyValuePair<string, string>);
+        }
+      }
+    });
+    this.allSubscriptions.push(bankAccountSubscription);
+  }
+
+  onPaymentStatusChange($event: MatRadioChange) {
+    var selection = $event.value;
+    this.expense.amountPaid = selection === 'Paid' 
+                              ? this.expense.price
+                              : 0;
   }
   
   onSave() {
@@ -47,8 +78,6 @@ export class ExpenseAddComponent implements OnDestroy, OnInit {
   }
 
   ngOnDestroy(): void {
-    if (this.subscription) {
-      this.subscription.unsubscribe();
-    }
+    this.allSubscriptions.forEach(s => { s.unsubscribe() });
   }
 }
