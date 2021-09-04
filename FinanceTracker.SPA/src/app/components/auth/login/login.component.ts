8c9d8f6aa10a2bc10c28d32ceef8cd32b58222d1
Ricardo Novais <ricardoo.developer@gmail.com>
2021-09-05 00:37:30 +0100

    Route import not being used

--- FinanceTracker.SPA/src/app/components/auth/login/login.component.ts
+++ FinanceTracker.SPA/src/app/components/auth/login/login.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 import { AuthService } from '../../../services/auth.service';
-import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { User } from 'src/app/models/user.model';
 import { CommonService } from 'src/app/services/common.service';
